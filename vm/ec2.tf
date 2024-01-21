8b061d51d7a770a35fd1ca938b42a45afbf206d3
student-ops <gogakuec@gmail.com>
2024-01-21 15:20:37 +0900

    update for k3sup

--- vm/ec2.tf
+++ vm/ec2.tf
@@ -5,7 +5,7 @@ variable "awsprops" {
     # ami        = "ami-04beabd6a4fb6ab6f" # AMAZON LINUX 2
     # ami          = "ami-0fda573abc329ed59" # ECS optimized AMAZON LINUX 2023
     ami          = "ami-07c589821f2b353aa" # UBUNTU 22.04
-    prod_itype   = "t2.micro"
+    prod_itype   = "t4g.small"
     publicip     = true
     keyname      = "senior_thesis"
     secgroupname = "mypotal_vpc_sg"
@@ -17,9 +17,17 @@ resource "aws_key_pair" "my_key" {
   public_key = file(var.public_key_path) # 変数を使用してパスを指定
 }
 
-resource "aws_instance" "secure_prog" {
+locals {
+  user_data = <<-EOF
+  #!/bin/bash
+  apt-get update -y
+  apt-get upgrade -y
+  EOF
+}
+
+resource "aws_instance" "k3s_server" {
   ami                         = var.awsprops["ami"]
-  instance_type               = var.awsprops["prod_itype"]
+  instance_type               = "t4g.medium"
   subnet_id                   = aws_subnet.my_subnet.id
   associate_public_ip_address = var.awsprops["publicip"]
   key_name                    = aws_key_pair.my_key.key_name
@@ -30,14 +38,9 @@ resource "aws_instance" "secure_prog" {
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-  user_data = <<-EOF
-            #!/bin/bash
-            apt-get update -y
-            apt-get upgrade -y
-            EOF
-
+  user_data = local.user_data
   root_block_device {
-    volume_size = 10
+    volume_size = 20
     volume_type = "gp2"
   }
 
@@ -51,11 +54,92 @@ resource "aws_instance" "secure_prog" {
   depends_on = [aws_security_group.mypotal_vpc_sg]
 }
 
-output "prod_ssh_command" {
-  value = "ssh -i ~/.ssh/myPotal ec2-user@${aws_eip.secure_ec2_eip.public_ip}" # Elastic IPを使用
+resource "aws_instance" "k3s_edge_worker" {
+  ami                         = var.awsprops["ami"]
+  instance_type               = var.awsprops["prod_itype"]
+  subnet_id                   = aws_subnet.my_subnet.id
+  associate_public_ip_address = var.awsprops["publicip"]
+  key_name                    = aws_key_pair.my_key.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.mypotal_vpc_sg.id
+  ]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = local.user_data
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name        = "k3s_edge_worker"
+    Environment = "DEV"
+    OS          = "UBUNTU"
+    Managed     = "IAC"
+  }
+
+  depends_on = [aws_security_group.mypotal_vpc_sg]
 }
 
-resource "aws_eip" "secure_ec2_eip" {
-  domain   = "vpc"
-  instance = aws_instance.secure_prog.id
+resource "aws_instance" "k3s_central_worker" {
+  ami                         = var.awsprops["ami"]
+  instance_type               = var.awsprops["prod_itype"]
+  subnet_id                   = aws_subnet.my_subnet.id
+  associate_public_ip_address = var.awsprops["publicip"]
+  key_name                    = aws_key_pair.my_key.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.mypotal_vpc_sg.id
+  ]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = local.user_data
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name        = "k3s_central_worker"
+    Environment = "DEV"
+    OS          = "UBUNTU"
+    Managed     = "IAC"
+  }
+
+  depends_on = [aws_security_group.mypotal_vpc_sg]
 }
+
+output "server_ssh_command" {
+  value = "ssh -i ~/.ssh/myPotal ubuntu@${aws_eip.k3s_server.public_ip}"
+}
+
+output "edge_worker_ssh_command" {
+  value = "ssh -i ~/.ssh/myPotal ubuntu@${aws_eip.k3s_edge_worker.public_ip}"
+}
+
+output "central_worker_ssh_command" {
+  value = "ssh -i ~/.ssh/myPotal ubuntu@${aws_eip.k3s_central_worker.public_ip}"
+}
+
+output "export_ip" {
+  value = "export IP_k3s_main=${aws_eip.k3s_server.public_ip}\nexport IP_k3s_edge_worker=${aws_eip.k3s_edge_worker.public_ip}\nexport IP_k3s_central_worker=${aws_eip.k3s_central_worker.public_ip}"
+}
+
+resource "aws_eip" "k3s_server" {
+  domain   = "vpc"
+  instance = aws_instance.k3s_server.id
+}
+
+resource "aws_eip" "k3s_edge_worker" {
+  domain   = "vpc"
+  instance = aws_instance.k3s_edge_worker.id
+}
+
+resource "aws_eip" "k3s_central_worker" {
+  domain   = "vpc"
+  instance = aws_instance.k3s_central_worker.id
+}
+
